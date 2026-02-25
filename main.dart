50d8c7270d06315ca33ea376e0c2214ecc36ee9f
saddam002 <mohammadsaddamali98@gmail.com>
2026-02-25 20:22:56 +0545

    Pushing code files to github

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  // BASIC DART VARIABLES
+  var name = "Voyager I";
+  var year = 1997;
+  var antennaDiameter = 3.7;
+
+  print("name: ${name}, year: ${year}, antena diameter: ${antennaDiameter}");
+
+  // OPERATORS
+}
