66e73d9ef69c0eae3a2db19976ef1305db157f54
saddam002 <mohammadsaddamali98@gmail.com>
2026-02-25 20:28:19 +0545

    learn operators and meaning

--- main.dart
+++ main.dart
@@ -7,4 +7,16 @@ void main(List<String> args) {
   print("name: ${name}, year: ${year}, antena diameter: ${antennaDiameter}");
 
   // OPERATORS
+  int x = 10;
+  int y = 10;
+  var z = x + y; //Addition
+  print(z);
+
+  print(x - y); // Substraction
+  print(x * y); // Multiply
+  print(x / y); // divide
+  print(x ~/ y); // divide and return integer result
+  print(x % y); // modulo(get the remainder)
+  print(x++); // increment by +1
+  print(x--); // decrement by -1
 }
